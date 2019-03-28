98fa3825d53a21d952c1b69f6c6eba867badefc7
petergdoyle <peter.doyle@travelport.com>
2019-03-28 02:11:15 -0600

    fixed script errors

--- spark_standalone_launch_master.sh
+++ spark_standalone_launch_master.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-$SPARK_HOME:/sbin/start-master.sh
+$SPARK_HOME/sbin/start-master.sh
--- spark_standalone_launch_worker_node.sh
+++ spark_standalone_launch_worker_node.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-$SPARK_HOME/bin spark-class org.apache.spark.deploy.worker.Worker  spark://localhost:7077 -c 4 -m 2048
+$SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker  spark://localhost:7077 -c 4 -m 2048
