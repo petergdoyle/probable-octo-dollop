89df1752ad03c4f747daaac06054b0d414b8aa68
petergdoyle <peter.doyle@travelport.com>
2019-03-29 04:55:28 -0600

    added data and scripts to make a demo for word count

--- spark_submit_KafkaWordCount.sh
+++ spark_submit_KafkaWordCount.sh
@@ -2,5 +2,4 @@
 java_class='challenge.java.spark.KafkaWordCount'
 
 mvn -f challenge/pom.xml clean install && \
-spark-submit \
-  --driver-java-options "-Dlog4j.configuration=file:///vagrant/spark_log4j_QUIET.properties"  --class $java_class /vagrant/challenge/target/challenge-1.0-SNAPSHOT-jar-with-dependencies.jar /vagrant/data/log_data.txt
+spark-submit --driver-java-options "-Dlog4j.configuration=file:///vagrant/spark_log4j_QUIET.properties"  --class $java_class /vagrant/challenge/target/challenge-1.0-SNAPSHOT-jar-with-dependencies.jar localhost:9092 kafka-word-count-consumer-group loremipsum
