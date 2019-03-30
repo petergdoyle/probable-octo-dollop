7c96a90006ce516f567de3b61ed169e8af1ea7ea
petergdoyle <peter.doyle@travelport.com>
2019-03-30 11:54:35 -0600

    reading from file, parsing, csv encoding, pushing to kafka !!!

--- challenge/src/main/java/challenge/java/RunKafkaMessageSender.java
+++ challenge/src/main/java/challenge/java/RunKafkaMessageSender.java
@@ -5,9 +5,16 @@ package challenge.java;
 
 import java.io.File;
 import java.io.InputStream;
-import java.util.HashMap;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -16,28 +23,50 @@ import java.util.Scanner;
 public class RunKafkaMessageSender {
 
     public static void main(String[] args) throws Exception {
-        
+
         //create a kafka producer 
-        InputStream resourceAsStream = RunKafkaMessageSender.class.getClassLoader().getResourceAsStream ("resources/kafka-0.10.2.1-producer.properties");
+        InputStream resourceAsStream = RunKafkaMessageSender.class.getClassLoader().getResourceAsStream("resources/kafka-0.10.2.1-producer.properties");
         Properties kafkaProducerProperties = new Properties();
         kafkaProducerProperties.load(resourceAsStream);
         KafkaMessageSender kafkaMessageSender = new KafkaMessageSender(kafkaProducerProperties);
-        
-        // get input data from file 
-        String filename = "/vagrant/data/log_data.txt";
-        if (args[0] != null && args[0].length() > 0) {
-            filename = args[0];
-        }
+
+        String REGEX_PATTERN = "^([\\d.]+) (\\S+) (\\S+) \\[([\\w:/]+\\s[+\\-]\\d{4})\\] \"(.+?)\" (\\d{3}) (\\d+) \"([^\"]+)\" \"([^\"]+)\"";
+        int NUM_FIELDS = 9;
+        Pattern PATTERN = Pattern.compile(REGEX_PATTERN);
+        SimpleDateFormat apacheLogFileTimestampFormat = new SimpleDateFormat("dd/MMM/yyyy:hh:mm:ss Z");
+
+        String filename = args[0];
         File file = new File(filename);
-        HashMap<String, Integer> ipCountMap = new HashMap<>();
         Scanner input = new Scanner(file);
-        int recordCount = 0;
-        // scan through the file and create new kafka messag from only what is needed 
         while (input.hasNextLine()) {
-            recordCount++;
-            LogParser lp = LogParserBuilder.createLogParser(input.nextLine());
-            String msg = lp.getSubNet().concat(",").concat(lp.getTimestamp().toString());
-            kafkaMessageSender.send(msg);
+            String nextLine = input.nextLine();
+            Matcher matcher = PATTERN.matcher(nextLine);
+            if (!matcher.matches() || NUM_FIELDS != matcher.groupCount()) {
+                throw new RuntimeException("Matches: " + matcher.matches() + " GroupCount" + matcher.groupCount() + " Bad Input " + nextLine);
+            }
+            String ip = matcher.group(1);
+            String[] parts = ip.split("\\.");
+            String subnet = parts[0].concat(".").concat(parts[1]).concat(".*.*");
+            String timestamp = matcher.group(4);
+            String logtime = "";
+            try {
+                Date parsedDt = apacheLogFileTimestampFormat.parse(timestamp);
+                logtime = new Timestamp(parsedDt.getTime()).toString();
+            } catch (ParseException ex) {
+                Logger.getLogger(LogRegex.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            String method = matcher.group(5);
+            String response = matcher.group(6);
+            String output = ip.concat(",")
+                    .concat(",")
+                    .concat(logtime)
+                    .concat(",")
+                    .concat(method)
+                    .concat(",")
+                    .concat(response)
+                    .concat(subnet);
+            System.out.println(output);
+            kafkaMessageSender.send(output);
         }
     }
 }
