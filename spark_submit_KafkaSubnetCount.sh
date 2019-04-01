7b0c9b35d91c9129ab65c68698e4b866056cdbf7
petergdoyle <peter.doyle@travelport.com>
2019-04-01 04:42:13 -0600

    correct the script to submit to the local spark cluster

--- spark_submit_KafkaSubnetCount.sh
+++ spark_submit_KafkaSubnetCount.sh
@@ -1,7 +1,11 @@
 
 java_class='challenge.java.spark.KafkaSubnetCount'
-
-# mvn -T 1C -f challenge/pom.xml --offline install  && \
+jar_name='/vagrant/challenge/target/challenge-1.0-SNAPSHOT-jar-with-dependencies.jar'
+broker_list='localhost:9092'
 topic_name="subnets"
 consumer_group_id="subnet-count-consumer-group"
-spark-submit --driver-java-options "-Dlog4j.configuration=file:///vagrant/spark_log4j_QUIET.properties"  --class $java_class /vagrant/challenge/target/challenge-1.0-SNAPSHOT-jar-with-dependencies.jar localhost:9092 $consumer_group_id $topic_name
+# mvn -T 1C -f challenge/pom.xml --offline install  && \
+spark-submit \
+  --master spark://localhost:7077 \
+  --driver-java-options "-Dlog4j.configuration=file:///vagrant/spark_log4j_QUIET.properties"  \
+  --class $java_class $jar_name $broker_list $consumer_group_id $topic_name
