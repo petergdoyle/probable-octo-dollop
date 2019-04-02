ce758d47f68ca28bb0352d5b414fe2f5804ee1f3
petergdoyle <peter.doyle@travelport.com>
2019-04-02 06:08:27 -0600

    fixed up build scripts and poms

---
+++ challenge-scala/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+mvn -T 1C clean install 
--- spark_submit_scala_LogReaderKafkaProducer.sh
+++ spark_submit_scala_LogReaderKafkaProducer.sh
@@ -4,11 +4,20 @@ driver_java_opts="--driver-java-options -Dlog4j.configuration=file:///vagrant/sp
 spark_cluster_address='spark://challenge.cleverfishsoftware.com:7077'
 class_name='com.cleverfishsoftware.challenge.scala.LogReaderKafkaProducer'
 jar='/vagrant/challenge-scala/target/challenge-scala-1.0-SNAPSHOT.jar'
-uber_jar='/vagrant/challenge-scala-uber-jar/target/challenge-uber-jar-1.0-SNAPSHOT-jar-with-dependencies.jar'
+uber_jar='/vagrant/challenge-scala-uber-jar/target/challenge-scala-uber-jar-1.0-SNAPSHOT-jar-with-dependencies.jar'
+driver_java_options='--driver-java-options "-Dlog4j.configuration=file:///vagrant/spark_log4j_QUIET.properties"'
 
 log_file='/vagrant/data/log_data.txt'
 broker_list='localhost:9092'
 topic_name="logs"
 
-mvn -f challenge-scala/pom.xml package && \
-spark-submit --master $spark_cluster_address --jars $uber_jar --class $class_name $jar $log_file $broker_list $topic_name
+skip_build="$1"
+status=0
+if  [ "$skip_build" != "--skipBuild" ]; then
+  mvn -f challenge-scala/pom.xml package
+  status=$?
+fi
+
+if test $status -eq 0; then
+  spark-submit $driver_java_options --master $spark_cluster_address --jars $uber_jar --class $class_name $jar $log_file $broker_list $topic_name
+fi
