567a4ea1d653026edc95e8d25b664817a83ee1f6
petergdoyle <peter.doyle@travelport.com>
2019-04-05 02:53:15 -0600

    reimplementing the frequency count detector using structured streaming

--- spark_submit_scala_SparkStreamingKafkaLogProcessor.sh
+++ spark_submit_scala_SparkStreamingKafkaLogProcessor.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+rdd_class_name='com.cleverfishsoftware.challenge.scala.SparkStreamingKafkaLogProcessorRDD'
+df_class_name='com.cleverfishsoftware.challenge.scala.SparkStreamingKafkaLogProcessorDF'
+
+class_name="$rdd_class_name"
 spark_cluster_address='spark://challenge.cleverfishsoftware.com:7077'
-class_name='com.cleverfishsoftware.challenge.scala.SparkStreamingKafkaLogProcessor'
 jar='/vagrant/challenge-scala/target/challenge-scala-1.0-SNAPSHOT.jar'
 uber_jar='/vagrant/challenge-scala-uber-jar/target/challenge-scala-uber-jar-1.0-SNAPSHOT-jar-with-dependencies.jar'
 driver_java_options='--driver-java-options "-Dlog4j.configuration=file:///vagrant/spark_log4j_QUIET.properties"'
