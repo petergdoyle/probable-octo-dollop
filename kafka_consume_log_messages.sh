eecf0b7692f136dcdc02c08e2c3427078bed02fd
petergdoyle <peter.doyle@travelport.com>
2019-04-05 04:19:40 -0600

    script cleanup

--- kafka_consume_log_messages.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-$KAFKA_HOME/bin/kafka-console-consumer.sh --new-consumer --bootstrap-server localhost:9092 --topic challenge-log-in  --from-beginning --delete-consumer-offsets
--- kafka_list_topics.sh
+++ kafka_list_topics.sh
@@ -1,19 +1,2 @@
 #!/usr/bin/env bash
-cd $(dirname $0)
-. ./bash_common.sh
-
-if [ -z $KAFKA_HOME ]; then
-  display_error "No env var KAFKA_HOME is set. Source your ~/.bash_profile or logout and log back in"
-  exit 1
-fi
-zk_host_port="localhost:2181"
-read -e -p "Enter the zk host/port: " -i "$zk_host_port" zk_host_port
-
-cmd="$KAFKA_HOME/bin/kafka-topics.sh --list --zookeeper $zk_host_port"
-display_command "$cmd"
-prompt=$BOLD$YELLOW"About to start List Topics as shown, continue? (y/n): $RESET"
-default_value="y"
-read -e -p "$(echo -e $prompt)" -i $default_value response
-if [ "$response" == 'y' ]; then
-  eval "$cmd"
-fi
+kafka-proxied/kafka/list_topics.sh
