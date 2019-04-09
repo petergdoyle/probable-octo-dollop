3162b234fa69ebe36bd5cecf2e498e3b97e4b515
petergdoyle <peter.doyle@travelport.com>
2019-04-08 19:18:48 -0600

    removed the bitnami docker kafka scripts

--- docker_kafka_cluster_destroy.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker-compose -f bitnami-docker-kafka/docker-compose.yml down
--- docker_kafka_cluster_start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-docker-compose -f bitnami-docker-kafka/docker-compose.yml up -d
-docker-compose -f bitnami-docker-kafka/docker-compose.yml ps
