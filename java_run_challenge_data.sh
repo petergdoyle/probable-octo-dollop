34b40fc274cf196dad36506137f246f6ee9d9b3c
petergdoyle <peter.doyle@travelport.com>
2019-04-11 10:22:33 -0600

    needed a path relative build step

--- java_run_challenge_data.sh
+++ java_run_challenge_data.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-java -cp target/challenge-1.0-SNAPSHOT.jar phdata.challenge.RunLogParser /vagrant/data/log_data.txt
+
+mvn -f challenge-java/pom.xml package && \
+java -cp challenge-java/target/challenge-1.0-SNAPSHOT.jar challenge.java.RunLogParser /vagrant/data/log_data.txt
